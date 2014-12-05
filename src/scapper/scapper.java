8b68f9376bd91f6b937a0cc5d2cf9453f08702c4
fallnomega <fallnomega@gmail.com>
2014-12-05 12:45:04 -0800

    v4
    added JFrame to the mix and provided results in a JTextArea instead of
    in a console.

--- src/scapper/scapper.java
+++ src/scapper/scapper.java
@@ -1,22 +1,28 @@
 package scapper;
 import com.jaunt.*; 
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-//import java.applet.Applet;
-//import java.awt.*;
-//import java.awt.image.*;
-//import java.io.*;
-//import java.net.URL;
-//import javax.imageio.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
 
 
 
 public class scapper {
-	public static class Scapper
+	//Scrapper class to pull /r/funny image links
+	public static class Scraper
 	{
 		private UserAgent userAgent = new UserAgent();//create userAgent (headless browser)
 		private ArrayList<String> list = new ArrayList(); //house all the /r/funny img links
-		Scapper(){
+		Scraper(){
 		}
 		
 		void setLink(){
@@ -28,35 +34,27 @@ public class scapper {
 			}
 		}
 		
+		ArrayList<String> getList(ArrayList<String> list){
+			list = this.list;
+			return list;
+		}
+		
 		String getLinks(){
 			String page = this.userAgent.doc.innerHTML(); //save HTML to String obj
 			int size = page.length();
-			//System.out.println("Size of page is: " + size);
 			int startPos = page.indexOf("thumbnail may-blank \" href=\"")+28;//find index position of first img tag
 			int sentinal = 0;
 			String link = this.listing(page, startPos,sentinal);
-			//int endPos = page.indexOf('"', startPos); //find end position of index where img link end based on next " char
-			//String link = page.substring(startPos,endPos); //retrieve img link based on start and end positions
 			return link;
 		}
 		
 		String listing(String page, int startPos, int sentinal){
 			
-			//System.out.println("First link at:  " + startPos);
 			int endPos = page.indexOf('"', startPos); //find end position of index where img link end based on next " char
 			String link = page.substring(startPos, endPos);//retrieve img link based on start and end positions
-			//System.out.println(link);
 			this.list.add(link);
-			
-			//startPos = endPos;
-			//startPos = page.indexOf(("thumbnail may-blank \" href=\""), endPos);
-			//System.out.println(startPos);
-			//System.out.println(page.substring(startPos, startPos + 60));
-			
 			int temp = page.indexOf(("thumbnail may-blank \" href=\""), endPos);
 			startPos = temp + 28;
-
-			//System.out.println("New startPos and endPos gives: " + page.substring(startPos, startPos +40));
 			if (sentinal == 25) return link;
 			sentinal++;
 			this.listing(page, startPos, sentinal);
@@ -65,23 +63,75 @@ public class scapper {
 		public void scapper(){
 	
 			this.setLink();//set the link
-			//System.out.println(this.userAgent.doc.innerHTML());//print the document as html
-			
-			//System.out.println("\n\nSettings: " + userAgent.settings);
-			//String title = userAgent.doc.findFirst("<title>").getText();
-			//System.out.println("\nReddits website title: " + title);
-
-			//String link = getLinks();
-			//System.out.println("getLink returns: " + link);
-			//this.list.add(getLinks());
 			getLinks();
-			//System.out.println("ArrayList list index 0 returns: " + this.list.get(0));
 			System.out.println("The image list returned is :");
 			for(String i : this.list){
 				System.out.println(i);
 			}	
 		}
 	}
+	
+	
+	//JFrame setup by subclass Scrape
+	static class Scrape extends JFrame {
 
-
+		Scrape(){
+			super("Scaper"); //calls JFrame contractor to set it up along with title
+			setLayout(new FlowLayout()); //set layout to flow
+			getContentPane().setBackground(Color.gray);//set background color to gray
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //terminate on close
+			/*
+			 * pack is used when you are using layout managers and you want the JFrame 
+			 * to be the smallest size showing all components. If you set the size of 
+			 * your JFrame manually ( setSize(), setBounds() ) then you don't need to 
+			 * call pack().
+			 */
+			this.pack();
+			
+			JLabel scrapeLabel = new JLabel("Click to scrape");//label	
+			add(scrapeLabel);//add label
+			
+			JButton scrape = new JButton("Scrape"); //Scrape button made
+			add(scrape,BorderLayout.CENTER); //add button to jframe instance	
+			
+			JTextArea textArea = new JTextArea(25,25);//create text area for links
+			add(textArea); //add text area to jframe
+		
+			
+			MyListener listener = new MyListener(textArea); //subclass of actionlistener
+			scrape.addActionListener(listener);//call MyListener(JTextArea text) constructor
+			
+			this.setLocationRelativeTo(null);//set location on window
+			this.setSize(500,500);//set size of frame
+			setVisible(true);//visible to user
+		}
+		
+		//actionlistener implemented by mylistener
+		public class MyListener implements ActionListener{
+		
+			private JTextArea text;//Listener instance test area
+			String link = "";//Listener instance link
+			ArrayList <String> listing = new ArrayList();//ArrayList to house Scraper links
+			
+			//call MyListener with JTextArea  and set text
+			public MyListener(JTextArea text){
+				this.text = text;
+			}
+			
+			//implement actionlistener on button click event
+			public void actionPerformed(ActionEvent e){
+				Scraper test = new Scraper();//build new Scraper obj
+				test.scapper();//run scraper
+				this.listing = test.getList(this.listing);//MyListener given links from scraper
+				
+				//for loop to append contents of ArrayList listing to String link
+				for(String i:this.listing){
+					this.link+= i + "\n";
+				}
+				this.text.setText(this.link);//set JTextArea to reflect imgur links
+				this.text.setEditable(false);//disable editing on the JTextArea results field
+			}
+		}
+		
+	}
 }
--- src/scapper/testDriver.java
+++ src/scapper/testDriver.java
@@ -1,35 +1,8 @@
 package scapper;
-
-import javax.swing.*;
-//import java.applet.Applet;
-//import java.awt.Graphics;
-//import java.awt.image.BufferedImage;
-//import java.io.IOException;
-//import java.net.URL;
-
-
-import javax.imageio.ImageIO;
-
-import scapper.scapper.Scapper;
+import scapper.scapper.Scrape;
 
 public class testDriver {
 	public static void main(String args[]){
-		Scapper test = new Scapper();
-		test.scapper();
-		HelloWorld hello = new HelloWorld();
-
-		
+		Scrape hello = new Scrape();
 	}
-	
-static class HelloWorld extends JFrame{
-		HelloWorld(){
-			JLabel jlbHelloWorld = new JLabel("Hello World");
-			add(jlbHelloWorld);
-			this.setSize(100,300);
-			setVisible(true);
-		}
-		
-	}
-
-
 }
